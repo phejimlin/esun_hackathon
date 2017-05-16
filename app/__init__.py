0b3f0334f4e77aab6d5a8e6c753e4479ba291c10
PHE <phejimlin@gmail.com>
2017-05-16 21:07:14 +0800

    Finish model.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ Licence: GPLv3
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
-# from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # from flask.ext.pymongo import PyMongo
 from flask.ext.login import LoginManager
 
@@ -14,11 +14,12 @@ app = Flask(__name__)
 
 #Configuration of application, see configuration.py, choose one and uncomment.
 #app.config.from_object('configuration.ProductionConfig')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://jim@esunmysql:1QAZ2wsx3edc@esunmysql.mysql.database.azure.com/esun'
 app.config.from_object('app.configuration.DevelopmentConfig')
 #app.config.from_object('configuration.TestingConfig')
 
 Bootstrap(app) #flask-bootstrap
-# db = SQLAlchemy(app) #flask-sqlalchemy
+db = SQLAlchemy(app) #flask-sqlalchemy
 
 lm = LoginManager()
 lm.init_app(app)
--- app/models.py
+++ app/models.py
@@ -4,34 +4,56 @@ Python Aplication Template
 Licence: GPLv3
 """
 
-# from app import db
-
-# class ModelExample(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	title = db.Column(db.String(250))
-# 	content = db.Column(db.Text)
-# 	date = db.Column(db.DateTime)
+from app import db
+import datetime
 
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     user = db.Column(db.String(64), unique = True)
-#     password = db.Column(db.String(500))
-#     name = db.Column(db.String(500))
-#     email = db.Column(db.String(120), unique = True)
-#     # posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+class User(db.Model):
+	ssn = db.Column(db.String(64), primary_key=True)
+	password = db.Column(db.String(500))
+	name = db.Column(db.String(500))
+	email = db.Column(db.String(120), unique=True)
 
-#     def is_authenticated(self):
-#         return True
+	def __init__(self, ssn, name, email):
+		self.ssn = ssn
+		self.name = name
+		self.email = email
 
-#     def is_active(self):
-#         return True
+	def is_authenticated(self):
+		return True
 
-#     def is_anonymous(self):
-#         return False
+	def is_active(self):
+		return True
 
-#     def get_id(self):
-#         return unicode(self.id)
+	def is_anonymous(self):
+		return False
 
-#     def __repr__(self):
-#         return '<User %r>' % (self.nickname)
+	def get_id(self):
+		return unicode(self.ssn)
+
+	def __repr__(self):
+		return '<User %r>' % (self.name)
+
+
+class Feedback(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	score = db.Column(db.Integer)
+	message = db.Column(db.Text)
+	created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+	from_user_id = db.Column(db.String(64), db.ForeignKey('user.ssn'))
+	from_who = db.relationship('User', foreign_keys=[from_user_id])
+	to_user_id = db.Column(db.String(64), db.ForeignKey('user.ssn'))
+	to_who = db.relationship('User', foreign_keys=[to_user_id])
+
+	def __init__(self, score, message, from_who, to_who, created_date=None):
+		self.score = score
+		self.message = message
+		if created_date is None:
+			created_date = datetime.datetime.utcnow()
+		self.from_who = from_who
+		self.from_user_id = from_who.ssn
+		self.to_who = to_who
+		self.to_user_id = to_who.ssn
+
+	def __repr__(self):
+		return '<Feedback %r>' % (self.message)
