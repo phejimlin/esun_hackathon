e662992131df5a11de51257fb0ad0d85b1b798fd
TobyOoO <tobbymailbox@gmail.com>
2017-05-19 14:51:31 +0800

    revise bag-of-words.

--- analysis/testing_dataset_classify.py
+++ analysis/testing_dataset_classify.py
@@ -11,7 +11,6 @@ import jieba
 import numpy as np
 
 from bs4 import BeautifulSoup
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
 
 
 # In[4]:
@@ -77,12 +76,15 @@ if __name__ == "__main__":
     dirs = os.listdir(folder_path)
 
 
-    corpus = []
+    corpus = list(np.load('top_n.npy'))
     comment_scores = []
     for doc in dirs:
         data = load_data(doc)
+        X_data = []
+        Y_data = []
         for seller, info in data.items():
             if len(info) != 0:
+                
                 for items in info:
                     for item in items:
                         score_type = item['point']
@@ -93,42 +95,21 @@ if __name__ == "__main__":
                                 comment_text = soup.getText()
                                 length = len(comment_text)
                                 if length > 13:
-                                    corpus.append(' '.join(jieba.cut(comment_text, cut_all=False)))
+                                    word_list = list(jieba.cut(comment_text, cut_all=False))
+                                    vec = []
+                                    for word in corpus:
+                                        value = 1 if word in word_list else 0
+                                        vec.append(value)
+                                    X_data.append(vec)
 
                                     if score_type == 'good':
-                                        comment_scores.append(1.0)
+                                        Y_data.append(1)
                                     if score_type == 'bad':
-                                        comment_scores.append(0.0)
+                                        Y_data.append(0)
+        np.save('X-{0}'.format(doc), np.array(X_data))
+        np.save('Y-{0}'.format(doc), np.array(Y_data))
+
         break
-
-
-
-    tfidf_vectorizer = TfidfVectorizer()
-    tfidf = tfidf_vectorizer.fit_transform(corpus)
-    tfidf_features = tfidf_vectorizer.get_feature_names()
-
-    sorted_scores = display_scores(tfidf_vectorizer, tfidf)
-
-    bag_of_words = [word[0] for word in sorted_scores[:198]]
-    top_n = np.array(bag_of_words)
-    np.save('top_n.npy', top_n)
-
-
-    testing_set = []
-    for sentence in corpus:
-        features = []
-        for word in bag_of_words:
-            if word in sentence.split(' '):
-                features.append(1)
-            else:
-                features.append(0)
-        testing_set.append(features)
-
-
-    X = np.array(testing_set)
-    Y = np.array(comment_scores)
-    np.save('ruten_X_testing.npy', X)
-    np.save('ruten_Y_testing.npy', Y)
 
 
 
