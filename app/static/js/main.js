72897d805b4ece529437665c50688076a0326424
PHE <phejimlin@gmail.com>
2017-05-20 20:31:14 +0800

    Finish query js and redirect.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,16 @@
 var currentURL = window.location;
 function init_person(){
+    var queryName = getParameterByName('other_ssn', currentURL.href)
+    var url = currentURL.origin + '/api/profile'
+
+    // For query other people!
+    if (queryName) {
+        url += '?other_ssn='+queryName
+    }
+
 	$.ajax({
         type:'GET',
-        url: currentURL.origin + '/api/profile',
+        url: url,
         success:function(response){
             result = response;
             console.log(result);
@@ -25,18 +33,29 @@ function init_person(){
 
 function search_submit(){
     var search_query = document.getElementById("input_v").value;
-    console.log(search_query);
-    console.log(currentURL);
+    var redirectUrl = currentURL.origin + '/index?other_ssn=' +search_query
+    var url = currentURL.origin + '/api/profile?other_ssn=' +search_query
     $.ajax({
         type:'GET',
-        url: currentURL.origin + '/api/profile?other_ssn=' +search_query,
+        url: url,
         success:function(response){
-            result = response;
-            console.log(response);
-            alert(result);
+            window.location = redirectUrl;
         },
         error:function(error){
             console.log(error);
+            if (error.status == 404){
+                alert("查無此人，請再次搜尋。")
+            }
         }
     });
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
