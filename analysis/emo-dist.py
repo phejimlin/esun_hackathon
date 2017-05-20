7cb670ce31feddf6c9a24a9eab8712cd674ddfe7
TobyOoO <tobbymailbox@gmai.com>
2017-05-20 17:50:48 +0800

    finalize user profile.

--- analysis/emo-dist.py
+++ analysis/emo-dist.py
@@ -3,36 +3,39 @@ import json
 from sklearn.preprocessing import normalize
 import numpy as np
 
-emo_data = json.load(open('emo_fb_comments2.json', 'r'))
+
 emo_list = ['wow', 'haha', 'angry', 'love', 'sad']
 user_emo = {}
-user_data = json.load('user_profile_ruten.json')
-for user_url, item in emo_data.items():
-	if len(item) == 0: continue
-	emo_freq = {}
-	for emo in emo_list:
-		emo_freq[emo] = 0
+user_data = json.load(open('user_profile_ruten.json', 'r'))
+for emo_file in ['emo_fb_comments5.json', 'emo_fb_comments2.json']:
+	emo_data = json.load(open(emo_file, 'r'))
+	for user_url, item in emo_data.items():
+		if len(item) == 0: continue
+		emo_freq = {}
+		for emo in emo_list:
+			emo_freq[emo] = 0
 
-	last_emo = []
-	emo_consist = []
-	for comment in item:
+		last_emo = []
+		emo_consist = []
+		for comment in item:
 
-		emo_freq[comment['emotion1']] += 1
-		emo_freq[comment['emotion2']] += 1
-		emo_consist += [1 if comment['emotion1'] in last_emo or comment['emotion2'] in last_emo else 0]
-		last_emo = [comment['emotion1'], comment['emotion2']]
+			emo_freq[comment['emotion1']] += 1
+			emo_freq[comment['emotion2']] += 1
+			emo_consist += [1 if comment['emotion1'] in last_emo or comment['emotion2'] in last_emo else 0]
+			last_emo = [comment['emotion1'], comment['emotion2']]
 
-	norm_emo_freq = normalize(np.array(list(emo_freq.values())).reshape(1, -1), axis=1)
-	emo_std = np.array(emo_consist).std()
-	
-	try:
-		user = user_url.split('?s=')[-1]
-		user_data[user]
-	except:
-		continue
+		norm_emo_freq = normalize(np.array(list(emo_freq.values())).reshape(1, -1), axis=1)
+		emo_std = np.array(emo_consist).std()
+		
+		try:
+			user = user_url.split('?s=')[-1]
+			user_data[user]
+		except:
+			print('err')
+			continue
 
-	user_data['norm_emo_freq'] = norm_emo_freq
-	user_data['emo_std'] = emo_std
+		user_data[user]['norm_emo_freq'] = list(norm_emo_freq[0])
+		user_data[user]['emo_std'] = float(emo_std)
 
 with open('user_profile_ruten_emo.json', 'w') as f:
     json.dump(user_data, f)
--- analysis/review-dist.py
+++ analysis/review-dist.py
@@ -4,25 +4,27 @@ from sklearn.preprocessing import normalize
 import numpy as np
 
 
-user_data = json.load('user_profile_ruten_emo.json')
+user_data = json.load(open('user_profile_ruten_emo.json', 'r'))
 for user_id, user in user_data.items():
-	if len(user) == 0: continue
-
+	if 'review_ruten' not in user.keys(): continue
+	#print(user)
 	review = {}
-	score = np.array(user['review_ruten']).astype('float32')
+	score = np.array(user['review_ruten']).astype('float32').reshape(1, -1)
 	l2_score = normalize(score)
-	review['review_mean'] = np.mean(score)
-	review['review_std'] = np.std(score)
-	review['review_median'] = np.median(score)
+	review['review_mean'] = float(np.mean(score))
+	review['review_std'] = float(np.std(score))
+	review['review_median'] = float(np.median(score))
 	review['review_num'] = len(user['review_ruten'])
 
 	threshold = 50/100
 	vfunc = np.vectorize(lambda x: 1.0 if x >= threshold else 0.0)
 
-	review['review_pos'] = np.sum(vfunc(score)) / review['review_num']
-
+	review['review_pos'] = int(np.sum(vfunc(score))) / review['review_num']
+	#print(review)
+	#print(score, l2_score)
+	#break
 	del user_data[user_id]['review_ruten']
-	user_data[user_id] += review
+	user_data[user_id].update(review)
 
 
 with open('user_profile_final.json', 'w') as f:
